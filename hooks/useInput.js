f8de6328c0a40ca69bf00062cbab628427235c05
San-Goon <tigerlove95@naver.com>
2022-08-12 22:09:32 +0900

    feat: modify useInput for handle Error

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -1,9 +1,17 @@
 import {useState, useCallback} from "react";
 
-export default (initialValue = null) => {
+export default (initialValue = null, min, max) => {
     const [value, setValue] = useState(initialValue)
+    const [error, setError] = useState(false)
     const handler = useCallback((e) => {
         setValue(e.target.value)
+        console.log(e.target.value, e.target.value.length, min, max)
+        if (e.target.value.length < min || e.target.value.length > max) {
+            setError(true)
+        } else {
+            setError(false)
+        }
     }, [])
-    return [value, handler]
+    console.log(error)
+    return [value, handler, error]
 }
