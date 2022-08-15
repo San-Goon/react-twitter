dbf906898a51332dc19f06d249c10f3b6af56061
San-Goon <tigerlove95@naver.com>
2022-08-15 17:03:22 +0900

    feat: split reducers by using combineReducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,74 +1,20 @@
 import {HYDRATE} from "next-redux-wrapper";
 
-const initialState = {
-    user : {
-        isLoggedIn: false,
-        user: null,
-        signUpData: {},
-        loginData: {},
+import user from './user';
+import post from './post';
+import {combineReducers} from "redux";
+
+const rootReducer = combineReducers({
+    index: (state = {}, action) => {
+        switch (action.type) {
+            case HYDRATE:
+                return {...state, ...action.payload}
+            default:
+                return state;
+        }
     },
-    post: {
-        mainPosts: [],
-    }
-}
-
-export const logoutAction = (data) => {
-    return {
-        type: "LOG_OUT",
-        data,
-    }
-}
-
-export const loginAction = (data) => {
-    return {
-        type: "LOG_IN",
-        data,
-    }
-}
-
-const changeNickname = (data) => {
-    return {
-        type: "CHANGE_NICKNAME",
-        data,
-    }
-}
-
-
-
-const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case HYDRATE:
-            return {...state, ...action.payload}
-
-        case "LOG_IN":
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: true,
-                    user: action.data,
-                }
-            }
-        case "LOG_OUT":
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: false,
-                    user: null,
-                }
-            }
-        case "CHANGE_NICKNAME":
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    name: action.data,
-                }
-            }
-        default:
-            return state;
-    }
-}
+    user,
+    post
+})
 
 export default rootReducer;
