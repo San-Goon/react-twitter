5a3ef6dc6aea99cfb39e2bbc549b0f2fa72fb13c
San-Goon <tigerlove95@naver.com>
2022-08-16 18:41:43 +0900

    feat: make PostCard, PostForm Components for post.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import AppLayout from "../components/AppLayout";
+import {useSelector} from "react-redux";
+import PostForm from "../components/PostForm";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
+    const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
+    const mainPosts = useSelector((state) => state.post.mainPosts)
     return (
         <AppLayout selected={"home"}>
-            <div>Hello, Next!</div>
+            {isLoggedIn && <PostForm />}
+            {mainPosts.map((post, index) => (<PostCard key={post.id} post={post} />))}
         </AppLayout>
     )
 }
--- reducers/post.js
+++ reducers/post.js
@@ -1,9 +1,54 @@
 export const initialState = {
-    mainPosts: [],
+    mainPosts: [{
+        id: 1,
+        User: {
+            id: 1,
+            nickname: "산군"
+        },
+        content: "하위 ^^ #하이",
+        Images: [{
+
+        }],
+        Comments: [{
+            User: {
+                nickname: "상남자"
+            },
+            content: "어험"
+        }, {
+            User: {
+                nickname: "협회장"
+            },
+            content: "^^"
+        }]
+    }],
+    imagePaths: [],
+    postAdded: false,
+}
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+    type: ADD_POST,
+}
+
+const dummyPost = {
+    id: 2,
+    content: "더미",
+    User: {
+        id: 1,
+        nickname: "산군"
+    },
+    Images: [],
+    Comments: [],
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_POST:
+            return {
+                ...state,
+                mainPosts: [dummyPost, ...state.mainPosts],
+                postAdded: true,
+            }
         default:
             return state;
     }
