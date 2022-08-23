95dbd55e1eee7632bbba69ab31180a93b690b568
San-Goon <tigerlove95@naver.com>
2022-08-24 08:46:57 +0900

    fix: change condition for load postForm ( logInDone -> me )

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
 
 const Home = () => {
-    const logInDone = useSelector((state) => state.user.logInDone)
+    const me = useSelector((state) => state.user.me)
     const mainPosts = useSelector((state) => state.post.mainPosts)
     return (
         <AppLayout selected={"home"}>
-            {logInDone && <PostForm />}
+            {me && <PostForm />}
             {mainPosts.map((post, index) => (<PostCard key={post.id} post={post} />))}
         </AppLayout>
     )
