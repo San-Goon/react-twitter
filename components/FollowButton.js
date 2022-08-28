53e62756b2971d930505eb1d0496362c9864d635
San-Goon <tigerlove95@naver.com>
2022-08-28 20:29:18 +0900

    feat: implement follow unfollow function by button.

--- components/FollowButton.js
+++ components/FollowButton.js
@@ -1,8 +1,44 @@
-const FollowButton = () => {
+import PropTypes from "prop-types";
+import {Button} from "antd";
+import {useCallback} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {FOLLOW_REQUEST, UNFOLLOW_REQUEST} from "../reducers/user";
+
+const FollowButton = ({post}) => {
+    const me  = useSelector((state) => state.user.me);
+    const followLoading = useSelector((state) => state.user.followLoading)
+    const unfollowLoading = useSelector((state) => state.user.unfollowLoading)
+
+    const dispatch = useDispatch();
+
+    const isFollowing = me?.Followings.find((v) => v.id === post.User.id);
+
+    const onClickButton = useCallback(() => {
+        if (isFollowing) {
+            dispatch({
+                type: UNFOLLOW_REQUEST,
+                data: post.User.id,
+            })
+        } else {
+            dispatch({
+                type: FOLLOW_REQUEST,
+                data: post.User.id,
+            })
+        }
+    },[isFollowing]);
+
     return (
-        <div>FollowButton</div>
+        <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
+            {isFollowing ? "언팔로우" : "팔로우"}
+        </Button>
     )
 }
 
+FollowButton.propTypes = {
+    post: PropTypes.object.isRequired,
+}
+
 export default FollowButton;
+
+
 
